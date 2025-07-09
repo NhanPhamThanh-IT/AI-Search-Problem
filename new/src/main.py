6c7c6da182bf940ebbad0c5bed06ef6d443af214
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2025-07-09 21:12:58 +0700

    Refactor code

---
+++ new/src/main.py
@@ -0,0 +1,23 @@
+import pygame
+from .config import SETTINGS
+from .states import run_menu, run_playing
+
+def run_game():
+    pygame.init()
+    screen = pygame.display.set_mode((SETTINGS["WINDOW_SIZE"][0], SETTINGS["WINDOW_SIZE"][1]))
+    pygame.display.set_caption(SETTINGS["TITLE"])
+    clock = pygame.time.Clock()
+
+    state = SETTINGS["SCENES"]["MENU"]
+    play_data = {}
+
+    running = True
+    while running:
+        if state == SETTINGS["SCENES"]["MENU"]:
+            state, play_data = run_menu(screen, clock)
+        elif state == SETTINGS["SCENES"]["PLAY"]:
+            state = run_playing(screen, clock, play_data)
+        elif state == SETTINGS["SCENES"]["QUIT"]:
+            running = False
+
+    pygame.quit()
---
+++ new/start.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Running Pygame project...
+python run.py
+pause
