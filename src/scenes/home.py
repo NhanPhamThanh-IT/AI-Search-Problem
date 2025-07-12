50a60369ef27331a73369292344a070c246dbcb5
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2025-07-12 15:07:29 +0700

    Update new UI

--- src/scenes/home.py
+++ src/scenes/home.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 from config import SETTINGS
 from entities import Button
 
@@ -7,27 +8,42 @@ class HomeScreen:
         self.screen = screen
         self.clock = clock
         self.font = pygame.font.Font(None, 36)
-        self.title_font = pygame.font.Font(None, 42)
+        self.title_font = pygame.font.Font(None, 48)
         self.screen_width, self.screen_height = SETTINGS["WINDOW_SIZE"]
 
+        try:
+            logo_path = os.path.join("assets", "logo.png")
+            self.logo = pygame.image.load(logo_path)
+            self.logo = pygame.transform.scale(self.logo, (300, 300))
+        except:
+            self.logo = None
+
+        button_width, button_height = 160, 60
+        button_x = self.screen_width // 2 - button_width // 2
+        button_y = int(self.screen_height * 0.75)
         self.play_button = Button(
-            rect=pygame.Rect(self.screen_width // 2 - 60, int(self.screen_height * 0.6), 120, 60),
+            rect=pygame.Rect(button_x, button_y, button_width, button_height),
             text="Play",
         )
 
     def draw(self):
         self.screen.fill(SETTINGS["BG_COLOR"])
-        
+
         title_surf = self.title_font.render("Rush Hour Solver", True, (255, 255, 255))
-        title_rect = title_surf.get_rect(center=(self.screen_width // 2, self.screen_height // 3))
+        title_rect = title_surf.get_rect(center=(self.screen_width // 2, int(self.screen_height * 0.1)))
         self.screen.blit(title_surf, title_rect)
+
+        if self.logo:
+            logo_x = self.screen_width // 2 - self.logo.get_width() // 2
+            logo_y = int(self.screen_height * 0.2)
+            self.screen.blit(self.logo, (logo_x, logo_y))
 
         self.play_button.draw(self.screen)
 
     def handle_events(self):
         mouse_pos = pygame.mouse.get_pos()
         self.play_button.hover = self.play_button.rect.collidepoint(mouse_pos)
-        
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return SETTINGS["SCENES"]["QUIT"], {}
